e7ab99c59bb9b1323ab9b9adc0d0a0239d1c2bc9
Student <kaczanowskamonika@gmail.com>
2019-09-01 14:50:20 +0100

    style: run linter

--- app.js
+++ app.js
@@ -1,84 +1,69 @@
 const fs = require("fs");
 
-const data = fs
-  .readFileSync("./src/input.txt")
-  .toString()
-  .split("\n");
-const roomData = data
-  .slice(0, 1)
-  .toString()
-  .split(" ");
-const hooverInitPosition = data
-  .slice(1, 2)
-  .toString()
-  .split(" ");
+const data = fs.readFileSync("./src/input.txt").toString().split("\n");
+const roomData = data.slice(0, 1).toString().split(" ");
+const hooverInitPosition = data.slice(1, 2).toString().split(" ");
 const dirtCoordinates = data.slice(2, 5);
 const directions = data[5].split("");
 
 class Hoover {
   constructor() {
     this.roomDimensions = { x: Number(roomData[0]), y: Number(roomData[1]) };
-    this.position = {
-      x: Number(hooverInitPosition[0]),
-      y: Number(hooverInitPosition[1])
-    };
-    this.dirtPatches = dirtCoordinates.map(dirt => {
-      let eachDirt = dirt.split(" ");
-      let dirtPatch = { x: 0, y: 0 };
-      dirtPatch.x = Number(eachDirt[0]);
-      dirtPatch.y = Number(eachDirt[1]);
-      return dirtPatch;
+    this.position = {x: Number(hooverInitPosition[0]), y: Number(hooverInitPosition[1])};
+    this.dirtPatches = dirtCoordinates.map(
+      dirt => {let eachDirt = dirt.split(" ");
+      let eachDirtPatch = { x: 0, y: 0 };
+      eachDirtPatch.x = Number(eachDirt[0]);
+      eachDirtPatch.y = Number(eachDirt[1]);
+      return eachDirtPatch;
     });
 
     this.directions = directions;
     this.dirtCounter = 0;
-    this.everyPosition = [];
+    // this.everyPosition = [];
   }
 
-moveHoover() {
-  this.directions.forEach(direction=> {
-    switch(direction){
-      case "N":
-      (this.position.y <= this.roomDimensions.y) && (this.position.y >= 0)
-          ? (this.position.y += 1)
-          : this.position.y
+  moveHoover() {
+    this.directions.forEach(direction => {
+      switch (direction) {
+        case "N":
+          this.position.y <= this.roomDimensions.y && this.position.y >= 0
+            ? (this.position.y += 1)
+            : this.position.y;
           break;
-          case "S":
-        (this.position.y <= this.roomDimensions.y)&& (this.position.y > 0)
-          ? (this.position.y -= 1)
-          : this.position.y;
-        break;
+        case "S":
+          this.position.y <= this.roomDimensions.y && this.position.y > 0
+            ? (this.position.y -= 1)
+            : this.position.y;
+          break;
         case "E":
-        (this.position.x <= this.roomDimensions.x) && (this.position.x > 0)
-          ? (this.position.x += 1)
-          : this.position.x;
-        break;
+          this.position.x <= this.roomDimensions.x && this.position.x > 0
+            ? (this.position.x += 1)
+            : this.position.x;
+          break;
         case "W":
-        (this.position.x <= this.roomDimensions.x)  && (this.position.x > 0)
-          ? (this.position.x -= 1)
-          : this.position.x;
-        break;
-    }
-    return this.cleanDirts();
-
-  })
-  return this.position
-}
-
-cleanDirts(){
-  this.dirtPatches.forEach((dirt, index) => {
-    if (dirt.x === this.position.x && dirt.y === this.position.y)
-      {
-        this.dirtCounter +=1
-        this.dirtPatches.splice(index,1)
+          this.position.x <= this.roomDimensions.x && this.position.x > 0
+            ? (this.position.x -= 1)
+            : this.position.x;
+          break;
       }
-  })
-  return this.dirtCounter
-}
+      return this.cleanDirts();
+    });
+    return this.position;
+  }
 
+  cleanDirts() {
+    this.dirtPatches.forEach((dirt, index) => {
+      if (dirt.x === this.position.x && dirt.y === this.position.y) {
+        this.dirtCounter += 1;
+        this.dirtPatches.splice(index, 1);
+      }
+    });
+    return this.dirtCounter;
+  }
 }
 
 module.exports = Hoover;
-const hoover = new Hoover
+const hoover = new Hoover();
 console.log(hoover.moveHoover());
 console.log(hoover.cleanDirts());
