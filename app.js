dfed765f924d35e2496d55bca8a56cc98be94b85
Student <kaczanowskamonika@gmail.com>
2019-09-01 12:51:46 +0100

    chore: create class Hoover

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("./src/input.txt").toString().split("\n");
-const roomData = data.slice(0, 1).toString().split(" ");
-const hooverInitPosition = data.slice(1, 2).toString().split(" ");
-const dirtCoordinates = data.slice(2, 5)
-const directions=data[5].split("");
+const data = fs
+  .readFileSync("./src/input.txt")
+  .toString()
+  .split("\n");
+const roomData = data
+  .slice(0, 1)
+  .toString()
+  .split(" ");
+const hooverInitPosition = data
+  .slice(1, 2)
+  .toString()
+  .split(" ");
+const dirtCoordinates = data.slice(2, 5);
+const directions = data[5].split("");
+
+class Hoover {
+  constructor() {
+    this.roomDimensions = { x: Number(roomData[0]), y: Number(roomData[1]) };
+    this.position = {
+      x: Number(hooverInitPosition[0]),
+      y: Number(hooverInitPosition[1])
+    };
+    this.dirtPatches = dirtCoordinates.map(dirt => {
+      let eachDirt = dirt.split(" ");
+      let dirtPatch = { x: 0, y: 0 };
+      dirtPatch.x = Number(eachDirt[0]);
+      dirtPatch.y = Number(eachDirt[1]);
+      return dirtPatch;
+    });
+
+    this.directions = directions;
+    this.dirtCounter = 0;
+    this.everyPosition = [];
+  }
+}
